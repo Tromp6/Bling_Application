d41a01275a3b596eed7fe4c1b122edc6fc2967a6
tromp6 <maxtrompetter@googlemail.com>
2022-11-03 14:51:59 -0700

    feat: add repository impl

---
+++ lib/age_guesser/data/repositories/age_guess_repository_impl.dart
@@ -0,0 +1,18 @@
+import '../../domain/entities/age_guess_entity.dart';
+import '../../domain/repositories/age_guess_repository.dart';
+import '../../domain/repositories/age_guess_result.dart';
+import '../datasources/remote_age_guess_data_source.dart';
+
+class AgeGuessRepositoryImpl implements AgeGuessRepository {
+  final RemoteAgeGuessDataSource remoteAgeGuessDataSource =
+      RemoteAgeGuessDataSource();
+  @override
+  Future<AgeGuessResult> fetchAgeGuess(String name) async {
+    try {
+      final result = await remoteAgeGuessDataSource.fetch(name);
+      return AgeGuessResult.data(AgeGuessEntity(age: result.age.toString()));
+    } catch (e) {
+      return AgeGuessResult.error(e as Exception);
+    }
+  }
+}
